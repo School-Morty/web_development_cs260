94191c6fdc07a65d9585a4df55ad87879023ecea
Tyler Mortensen <mortensen.tyler@gmail.com>
2020-03-12 17:29:58 -0600

    added some of the routes.

--- creative-workspace/src/router/index.js
+++ creative-workspace/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import News from '../views/News.vue'
+import Team from '../views/Team.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,22 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/news',
+    name: 'News',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: News
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component: Team
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
   }
 ]
 
